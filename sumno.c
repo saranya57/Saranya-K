61b8637191055b8ca7cce86b88bfb2ec77d96c1c
saranya57 <k.saranya57@gmail.com>
2017-07-12 16:37:34 +0530

    Create sumno.c

---
+++ sumno.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int no,a,s;
+clrscr();
+printf("enter the number\n");
+scanf("%d\n",&n);
+for(a=0;a<n;a++)
+{
+s=s+n;
+}
+printf("the sum",s);
+getch();
+}
