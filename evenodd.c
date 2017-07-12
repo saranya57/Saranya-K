dbab1e22442883ce4cacd9480e5a87fb46b33959
saranya57 <k.saranya57@gmail.com>
2017-07-12 13:27:33 +0530

    Create evenodd.c

---
+++ evenodd.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int no;
+clrscr();
+printf("enter the value for n \n");
+scanf("%d \n",&n);
+if(n%2)
+printf("the number is even\n");
+else
+printf("the number is odd\n");
+getch();
+}
