2e5ecd3a23bd9f8681901a5ce99b5d3714fd77a4
saranya57 <k.saranya57@gmail.com>
2017-07-12 16:17:48 +0530

    Create characterfind.c

---
+++ characterfind.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char t;
+clrscr();
+printf("enter the character\n");
+scanf("%d \n");
+if(t<'a'||t>'z')
+printf("the character is alphabet\n");
+elseif(t<'A'||t>'z');
+printf("the character is alphabet");
+else
+printf("the character is not alphabet");
+getch();
+}
