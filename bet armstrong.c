0cd80ec24d9d73c83b22944740679546fd3514d2
saranya57 <k.saranya57@gmail.com>
2017-07-15 09:16:40 +0530

    Create bet armstrong.c

---
+++ bet armstrong.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+int l,h,i,t1,t2,b,n=0,r=0;
+clrscr();
+printf("enter the two numbers");
+scanf("%d   %d",&l,&h);
+printf( "the armstrong numbers between two intervals are ");
+if(i=l+1;i<h;i++)
+{
+t2=i;
+t1=i;
+}
+while(t1!=0)
+{
+t1/=10;
+++n;
+}
+while(temp2!=0)
+{
+b=t2%10;
+r=pow(b,n);
+t2/=10;
+}
+ir(r==i)
+{
+printf(" %d",i);
+}
+n=0;
+r=0;
+}
+return 0;
+getch();
+}
