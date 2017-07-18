176e20887ebcf1867732de6c0d19e421be76b400
saranya57 <k.saranya57@gmail.com>
2017-07-18 09:38:23 +0530

    Create reverse str.c

---
+++ reverse str.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+char s[100],r[100];
+clrscr();
+printf("enter the string \n");
+scanf("%s\n",&s);
+printf("the given string is",s);
+for(a=0;str[a]!=\0;a++)
+{
+c=a-1;
+}
+for(b=0;b<=a-1;b++)
+{
+r[b]=s[c];
+}
+printf("the reversed string is ",r);
+getch();
+}
