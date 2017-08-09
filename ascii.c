f2278a3fdc904414ee99b0b7bca7a7b50d89432d
saranya57 <k.saranya57@gmail.com>
2017-08-09 16:37:06 +0530

    Add files via upload

---
+++ ascii.c
@@ -0,0 +1,17 @@
+ 
+
+#include<stdio.h>
+#include<conio.h> 
+ 
+void main()
+{
+int i=0;
+clrscr();
+while(i<=255)
+{
+printf("%d %c\n",i,i); 
+i++;
+}
+printf("\n press any key to proced");
+getch();
+}
